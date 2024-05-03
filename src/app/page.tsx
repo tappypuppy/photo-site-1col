96e876db16a8f8523ff8a69a41a4a13c340d39bb
Kazuki Takahashi <narita30830613@gmail.com>
2024-05-03 22:38:14 +0900

    リファクタリング

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import Image from "next/image";
+import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
 
 export default function Home() {
   return (
-    <div>hello world</div>
+    <div>
+      <Header />
+      <main>
+        <Hero />
+        <div>hello world</div>
+      </main>
+    </div>
   );
 }
