309998cdd776a589d21b1dafe52a74720b13ab14
Kazuki Takahashi <narita30830613@gmail.com>
2024-05-04 00:21:16 +0900

    indexを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
+import Index from "../components/Index/Index";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <main>
         <Hero />
+        <Index />
         <div>hello world</div>
       </main>
     </div>
---
+++ src/components/Index/Index.tsx
@@ -0,0 +1,30 @@
+import styles from "./Index.module.css";
+
+function Index() {
+  return (
+    <section className={styles.index_box}>
+      <div className={styles.inner}>
+        <h2 className={styles.title}>INDEX</h2>
+        <ol>
+          <li>
+            タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
+          </li>
+          <li>
+            タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
+          </li>
+          <li>
+            タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
+          </li>
+          <li>
+            タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
+          </li>
+          <li>
+            タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
+          </li>
+        </ol>
+      </div>
+    </section>
+  );
+}
+
+export default Index;
