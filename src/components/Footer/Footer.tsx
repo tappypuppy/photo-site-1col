1575a9488d528f40c727b8e95e75ea997bd88dd2
Kazuki Takahashi <narita30830613@gmail.com>
2024-05-04 15:27:21 +0900

    add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,13 @@
-import styles from './Footer.module.css';
+import styles from "./Footer.module.css";
 
 function Footer() {
-    return (
-        <footer className={styles.footer_box}>
-            Enter
-        </footer>
-    );
+  return (
+    <footer className={styles.footer_box}>
+      <div className={styles.inner}>
+        <p>© 2020 PHOTO BOOK</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
